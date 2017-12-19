59b2bba20123a10aea9273f714fbc772ab88595b
cogle <cogle@vt.edu>
2017-12-18 21:28:44 -0500

    checkpoint

--- model.py
+++ model.py
@@ -12,12 +12,12 @@ class lm(nn.Module):
 
     def init_weights(self, init_range=0.1):
         self.encoder.weight.data.uniform_(-init_range, init_range)
-        self.decoder.weight.data.uniform_(-init_range, init_range)
-        self.decoder.bias.data.fill_(0)
+        self.projection.weight.data.uniform_(-init_range, init_range)
+        self.projection.bias.data.fill_(0)
 
     def init_hidden(self, batch_size):
         weight = next(self.parameters()).data
-        wargs = (self.n_layers, batch_size, self.n_hidden)
+        wargs = (self.n_layers, batch_size, self.d_hidden)
         if self.rnn_type == 'LSTM':
             hidden = (Variable(weight.new(*wargs).zero_()), 
                       Variable(weight.new(*wargs).zero_()))
@@ -34,29 +34,43 @@ class lm(nn.Module):
         else:
             return tuple(lm.repackage_hidden(v) for v in h)
 
-    def __init__(self, n_vocab, d_embed, rnn_type, n_hidden, n_layers, p_drop):
+    def __init__(self, n_vocab, d_embed, rnn_type, d_hidden, n_layers, p_drop):
         super(lm, self).__init__()
         self.n_vocab = n_vocab
         self.d_embed = d_embed
         self.rnn_type = rnn_type
-        self.n_hidden = n_hidden
+        self.d_hidden = d_hidden
         self.n_layers = n_layers
         self.p_drop = p_drop
+
         self.encoder = nn.Embedding(self.n_vocab, self.d_embed)
         if self.rnn_type in ('LSTM', 'GRU'):
             rnn_cls = getattr(nn, self.rnn_type)
-            self.rnn = rnn_cls(self.d_embed, self.n_hidden, self.n_layers, 
+            self.rnn = rnn_cls(self.d_embed, self.d_hidden, self.n_layers, 
                                dropout=self.p_drop)
-        self.decoder = nn.Linear(self.n_hidden, self.n_vocab)
+        self.projection = nn.Linear(self.d_hidden, self.n_vocab)
+        self.decoder = nn.LogSoftmax(dim=None)
+
         self.init_weights()
         self.epochs = 0
 
-    def forward(self, i, hidden):
-        embedded = self.encoder(i)
-        output, hidden = self.rnn(embedded, hidden)
-        osize = output.size(0) * output.size(1)
-        decoded = self.decoder(output.view(osize, output.size(2)))
-        return decoded.view(osize, decoded.size(1)), hidden
+    def forward(self, i, h):
+        #        i : (bptt, batch_size)
+        e = self.encoder(i)
+        # embedded : (bptt, batch_size, d_embed)
+        o, h = self.rnn(e, h)
+        #   output : (bptt, batch_size, d_hidden)
+        #   hidden : ((n_layers, batch_size, d_hidden), 
+        #             (n_layers, batch_size, d_hidden))
+        p = self.projection(o.view(o.size(0) * o.size(1), o.size(2)))
+
+        pdb.set_trace()
+
+        d = self.decoder(p)
+
+        pdb.set_trace()
+
+        return d.view(o.size(0) * o.size(1), d.size(1)), h
 
     def score(self, sentence, n=35):
         self.eval()
